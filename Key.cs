317fb9e9c7d1e37d822c803c3eb71cad4039b67f
Lorenzo Dick Schadeck <112909674+LorenzoSchadeck@users.noreply.github.com>
2024-05-29 13:57:35 -0300

    Update Key.cs

--- Key.cs
+++ Key.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    [SerializeField] private float interactionDistance = 2f;
-
     public void Interact()
     {
         Inventory.Instance.AddKey(this);
